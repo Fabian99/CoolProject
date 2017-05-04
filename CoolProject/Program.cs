e675eb8d1a41838156e2113534320b9aa0c35f87
admin <admin@A2096GOS10>
2017-05-04 09:09:36 +0200

    Add project files.

---
+++ CoolProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
